21090eb035cceab5e76e2c2189ef83135951cdbd
Dmitry <dmitri.bohdan@gmai.com>
2021-01-06 18:30:58 +0300

    Init commit

---
+++ AutoPark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoPark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
